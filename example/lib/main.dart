f0ef84cd89a6ad0d795b841889ae605f4e69c105
flyingtime <flyingtimeice@gmail.com>
2021-06-03 15:45:26 +0800

    完成null safety的迁移

---
+++ example/lib/main.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_install_plugin/flutter_install_plugin.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => new _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String _appUrl = '';
+  String _apkFilePath = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: const Text('Plugin example app'),
+        ),
+        body: new Column(
+          children: <Widget>[
+            TextField(
+              decoration: InputDecoration(
+                  hintText:
+                      'apk file path to install. Like /storage/emulated/0/demo/update.apk'),
+              onChanged: (path) => _apkFilePath = path,
+            ),
+            TextButton(
+                onPressed: () {
+                  onClickInstallApk();
+                },
+                child: Text('install')),
+            TextField(
+              decoration:
+                  InputDecoration(hintText: 'URL for app store to launch'),
+              onChanged: (url) => _appUrl = url,
+            ),
+            TextButton(
+                onPressed: () => onClickGotoAppStore(_appUrl),
+                child: Text('gotoAppStore'))
+          ],
+        ),
+      ),
+    );
+  }
+
+  void onClickInstallApk() async {
+    if (_apkFilePath.isEmpty) {
+      print('make sure the apk file is set');
+      return;
+    }
+
+    await Permission.storage.request();
+    if (await Permission.storage.request().isGranted) {
+      FlutterInstallPlugin.installApk(
+              _apkFilePath, 'com.zaihui.installpluginexample')
+          .then((result) {
+        print('install apk $result');
+      }).catchError((error) {
+        print('install apk error: $error');
+      });
+    } else {
+      print('Permission request fail!');
+    }
+  }
+
+  void onClickGotoAppStore(String url) {
+    url = url.isEmpty
+        ? 'https://itunes.apple.com/cn/app/%E5%86%8D%E6%83%A0%E5%90%88%E4%BC%99%E4%BA%BA/id1375433239?l=zh&ls=1&mt=8'
+        : url;
+    FlutterInstallPlugin.gotoAppStore(url);
+  }
+}
